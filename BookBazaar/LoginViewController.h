9116b8185514b336f79a9547ba01f3e747651c42
Wade Wilkey <wadewilkey@Wades-MacBook-Pro.local>
2014-10-28 13:00:13 -0600

    Initial commit

---
+++ BookBazaar/LoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.h
+//  BookBazaar
+//
+//  Created by Wade Wilkey on 23/10/2014.
+//  Copyright (c) 2014 Wade Wilkey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+
+@end
---
+++ BookBazaar/LoginViewController.m
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.m
+//  BookBazaar
+//
+//  Created by Wade Wilkey on 23/10/2014.
+//  Copyright (c) 2014 Wade Wilkey. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
