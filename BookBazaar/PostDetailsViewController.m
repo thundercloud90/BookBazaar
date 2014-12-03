84890bb229e099a61248289a4880682d4a085b98
Wade Wilkey <wadewilkey@Wades-MacBook-Pro.local>
2014-12-02 20:23:04 -0700

    Changed to 5s window size

--- BookBazaar/PostDetailsViewController.m
+++ BookBazaar/PostDetailsViewController.m
@@ -41,7 +41,6 @@
 }
 
 
-
 -(void)downloadItems
 {
     //NSString * username = self.uNameTextField.text;
@@ -76,14 +75,5 @@
     
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
